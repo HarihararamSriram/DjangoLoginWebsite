fb6413306f93870d495ac2d002f1edd455ab473c
HarihararamSriram <harisriram2002@gmail.com>
2021-08-15 09:54:52 +0530

    Final commit (maybe, 1)

--- proj1/settings.py
+++ proj1/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6t3^d$=(0+4jb7v67a1qn!xl@)8fo%f^vyl5s*=%y9r(^_c#kv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'https://djangologinwebsite.herokuapp.com/', '*']
 
