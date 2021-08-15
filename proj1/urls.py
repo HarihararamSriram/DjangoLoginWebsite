973840312e943f4b5e4c311a77aada9a994342c0
HarihararamSriram <harisriram2002@gmail.com>
2021-08-15 09:25:38 +0530

    Final commit (maybe)

--- proj1/urls.py
+++ proj1/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 
 from django.views.static import serve
 from django.conf.urls import url
+from . import settings
 urlpatterns = [
     path('', include('websiteproj.urls')),
     path('admin/', admin.site.urls),\
